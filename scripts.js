e0a8b77e27fe131fe7d47a5cd1850ced2d400b18
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-10-14 21:25:21 -0500

    Added window load and flight status change in 2b

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    console.log('window loaded');
+
+    let takeOff = document.getElementById("takeoff");
+    takeOff.addEventListener("click", function (event) {
+        // console.log("Take off")
+        // console.log(event);
+        let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (response) {
+            let flightStatus = document.getElementById("flightStatus");
+            flightStatus.innerHTML = "Shuttle in flight.";
+        }
+    });
+});
