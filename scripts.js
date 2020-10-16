a520fd5406fe63771c51f347b8a212f642b1b348
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-10-16 14:13:57 -0500

    finished step 3

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
     console.log('window loaded');
-
+    // "Take off" button events
     let takeOff = document.getElementById("takeoff");
     takeOff.addEventListener("click", function (event) {
         // console.log("Take off")
@@ -14,5 +14,16 @@ window.addEventListener("load", function() {
             let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
             spaceShuttleHeight.innerHTML = 10000;
         }
+    });
+    // "Land" button events
+    let land = document.getElementById("landing");
+    land.addEventListener("click", function (event) {
+        let response = window.alert("The shuttle is landing. Landing gear engaged.");
+        let flightStatus = document.getElementById("flightStatus");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        let shuttleBackground = document.getElementById("shuttleBackground");
+        shuttleBackground.style.backgroundColor = "green";
+        let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+        spaceShuttleHeight.innerHTML = 0;
     });
 });
