014da6fbc558508ade3e3a8510aef41443515136
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-10-18 19:23:52 -0500

    step 5 complete

--- scripts.js
+++ scripts.js
@@ -39,16 +39,45 @@ window.addEventListener("load", function() {
             spaceShuttleHeight.innerHTML = 0;
         }
     });
-    // Move rocket image up
     // How to add an event listener to a button that does not have an id
+    // Assign a 'position: absolute' for "rocket" and center
+    let rocket = document.getElementById("rocket");
+    rocket.style.position = "absolute";
+    rocket.style.bottom = 0;
+    rocket.style.left = "178px";
+    let bottom = rocket.style.bottom;
+    bottom = bottom.replace("px","");
+    let center = rocket.style.left;
+    center = center.replace("px","");
     let allButtons = document.getElementsByTagName("button");
+    // Move rocket image up
     let moveUp = allButtons[0];
     moveUp.addEventListener("click", function (event) {
-        let rocket = document.getElementById = "rocket"
-        console.log(rocket.height);
+        bottom = Number(bottom) + 10;
+        rocket.style.bottom = bottom + "px";
+        let spaceShuttleHeight = document.getElementById("spaceShuttleHeight").innerHTML;
+        spaceShuttleHeight = Number(spaceShuttleHeight) + 10000;
+        document.getElementById("spaceShuttleHeight").innerHTML = spaceShuttleHeight;
     });
-    // let moveDown = allButtons[1];
-    // let moveRight = allButtons[2];
-    // let moveLeft = allButtons[3];
-    
+    // Move rocket image down
+    let moveDown = allButtons[1];
+    moveDown.addEventListener("click", function (event) {
+        bottom = Number(bottom) - 10;
+        rocket.style.bottom = bottom + "px";
+        let spaceShuttleHeight = document.getElementById("spaceShuttleHeight").innerHTML;
+        spaceShuttleHeight = Number(spaceShuttleHeight) - 10000;
+        document.getElementById("spaceShuttleHeight").innerHTML = spaceShuttleHeight;
+    });
+    // Move rocket image right
+    let moveRight = allButtons[2];
+    moveRight.addEventListener("click", function (event) {
+        center = Number(center) + 10;
+        rocket.style.left = center + "px";
+    });
+    // Move rocket image left
+    let moveLeft = allButtons[3];
+    moveLeft.addEventListener("click", function (event) {
+        center = Number(center) - 10;
+        rocket.style.left = center + "px";
+    });
 });
